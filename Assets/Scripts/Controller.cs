d9ede6c81cab4d1740d5edb2067fd380855c0efd
Hasan Demirtas <61328406+hasandemirtas@users.noreply.github.com>
2022-06-21 23:29:52 +0300

    start scene

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controller : MonoBehaviour
+{
+    public void btnSumOnClick()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
