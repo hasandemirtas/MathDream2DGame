e55eb3a814d7e10e102180f25d5e0212a429c735
safakcimenli <safakcimenli@gmail.com>
2022-06-23 12:34:03 +0300

    commit
    Movement has been improved

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,34 +4,88 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float JumpForce = 10f;
-    public float JumpForceLimit = 3;
+    //public float JumpForce = 10f;
+    //public float JumpForceLimit = 3;
 
-    private Rigidbody2D rb;
+    //private Rigidbody2D rb;
+    //void Start()
+    //{
+    //    rb = GetComponent<Rigidbody2D>();
+    //}
+
+    //void FixedUpdate()
+    //{
+
+    //    if (Input.GetKey(KeyCode.UpArrow) && rb.velocity.y < JumpForceLimit)
+    //    {
+    //        rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+    //    }
+
+    //    if (Input.GetKey(KeyCode.RightArrow) && rb.velocity.x < JumpForceLimit)
+    //    {
+    //        rb.AddForce(new Vector2(JumpForce / 2, JumpForce / 2), ForceMode2D.Impulse);
+    //    }
+
+    //    if (Input.GetKey(KeyCode.LeftArrow))
+    //    {
+    //        if (rb.velocity.x > 0)
+    //            rb.AddForce(new Vector2(-JumpForce / 2, JumpForce / 2), ForceMode2D.Impulse);
+    //        else if (Mathf.Abs(rb.velocity.x) < JumpForceLimit)
+    //            rb.AddForce(new Vector2(-JumpForce / 2, JumpForce / 2), ForceMode2D.Impulse);
+    //    }
+    public float xdirection;
+    public float ydirection;
+    public Rigidbody2D RB;
+    public float speed=12;
+    public float JumpConstant=17;
+    public bool isGround = false;
+
+    public void MoveHorizontal()
+    {
+        xdirection = Input.GetAxis("Horizontal");
+        ydirection = Input.GetAxis("Vertical");
+        RB.velocity = new Vector2(xdirection * speed, RB.velocity.y);
+        if (ydirection == 0)
+        {
+            isGround = true;
+        }
+        else
+        {
+            isGround = false;
+        }
+
+    }
+    public void Jump()
+    {
+        ydirection = Input.GetAxis("Vertical");
+        RB.velocity = new Vector2(RB.velocity.x, JumpConstant);
+
+    }
+    public void AcceleratedJump()
+    {
+        ydirection = Input.GetAxis("Vertical");
+        RB.AddForce(new Vector2(0, JumpConstant), ForceMode2D.Impulse);
+    }
+    // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        RB.GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
+    // Update is called once per frame
+    void Update()
     {
-
-        if (Input.GetKey(KeyCode.UpArrow) && rb.velocity.y < JumpForceLimit)
+        MoveHorizontal();
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
-        }
+            if (isGround)
+            {
+                AcceleratedJump();
+                //Jump();
 
-        if (Input.GetKey(KeyCode.RightArrow) && rb.velocity.x < JumpForceLimit)
-        {
-            rb.AddForce(new Vector2(JumpForce / 2, JumpForce / 2), ForceMode2D.Impulse);
-        }
+            }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            if (rb.velocity.x > 0)
-                rb.AddForce(new Vector2(-JumpForce / 2, JumpForce / 2), ForceMode2D.Impulse);
-            else if (Mathf.Abs(rb.velocity.x) < JumpForceLimit)
-                rb.AddForce(new Vector2(-JumpForce / 2, JumpForce / 2), ForceMode2D.Impulse);
         }
     }
 }
+
